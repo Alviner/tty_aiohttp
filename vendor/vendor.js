f44d2f0b75bfc2d5bbbb088b6bc35fafeacc9b51
Ivan Sitkin <alvinera@yandex.ru>
2025-02-27 22:20:29 +0100

    web tty

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -7,12 +7,6 @@ import "@xterm/xterm/css/xterm.css";
 
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 
-import {
-  createRouter,
-  createWebHistory,
-  RouterLink,
-} from "vue-router/dist/vue-router.esm-bundler.js";
-
 import elementPlusLocale from "element-plus/es/locale/lang/en.mjs";
 import elementPlus, {
   ElLoading as Loading,
@@ -20,21 +14,15 @@ import elementPlus, {
   ElNotification as Notification,
 } from "element-plus";
 
-import { v4 as uuidv4 } from "uuid";
-
 export {
   WSRPC,
   Terminal,
   FitAddon,
   WebglAddon,
   createApp,
-  createWebHistory,
-  createRouter,
-  RouterLink,
   elementPlus,
   elementPlusLocale,
   Loading,
   Message,
   Notification,
-  uuidv4,
 };
